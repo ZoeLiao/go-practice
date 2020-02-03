1bc000d68953b6ef63a1bd92005dc1faffe534be
ZoeLiao <zoejoyuliao@gmail.com>
2020-02-04 00:30:18 +0800

    Finish bubbleSort of 05_interface.go

--- src/05_interface.go
+++ src/05_interface.go
@@ -2,6 +2,7 @@ package main
 
 import "fmt"
 
+// duck typing
 type Sort interface {
     swap()
 }
@@ -12,11 +13,12 @@ type bubbleSort struct {
 
 func (b bubbleSort) swap() {
     for i:=0; i<len(b.array)-1; i++ {
-        for j:=1; j<len(b.array); j++ {
-            if b.array[i] > b.array[j] {
-                b.array[i], b.array[j] = b.array[j], b.array[i]
+        for j:=0; j<len(b.array)-1-i; j++ {
+            if b.array[j] > b.array[j+1] {
+                b.array[j], b.array[j+1] = b.array[j+1], b.array[j]
             }
         }
+        fmt.Println(b.array)
     }
 }
 
@@ -27,11 +29,17 @@ func f(sort Sort){
 }
 
 func main() {
-    array := make([]int, 5)
-    fmt.Println("enter marks of 5 subjects")
-    for i:=0; i<5; i++ {
+    var n int
+    fmt.Println("enter the length of array")
+    fmt.Scan(&n)
+
+    array := make([]int, n)
+    fmt.Printf("enter %d subjects:\n", n)
+    for i:=0; i<n; i++ {
         fmt.Scan(&array[i])
     }
+
     sort := bubbleSort{array: array}
+    fmt.Println("Bubble Sort")
     f(sort)
 }
