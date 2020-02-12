db6f3e30f87cd9ef2e5053727622ccb641ea9f22
ZoeLiao <zoejoyuliao@gmail.com>
2020-02-12 23:49:38 +0800

    Add 06_error.go

---
+++ src/06_error.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+    "errors"
+    "fmt"
+)
+
+
+type argError struct {
+    desc string
+    arg int
+}
+
+func (e *argError) Error() string {
+    return fmt.Sprintf("%s: %d", e.desc, e.arg)
+}
+
+func get_remainder(num1 int, num2 int) (int, error) {
+    // errors.New constructs a basic error value with the given error message
+    if num1 < 0 {
+        return -1, errors.New("Error1: can't work with negative number")
+    }
+    // use custom types as errors by implementing the Error() method on them
+    if num2 < 0 {
+        return -1,  &argError{"Error2: can't work with negative number", num2}
+    }
+
+    return num1 % num2, nil
+}
+
+
+func main() {
+    var n1, n2 int
+    fmt.Println("enter the dividend:")
+    fmt.Scan(&n1)
+    fmt.Println("enter the divisor:")
+    fmt.Scan(&n2)
+
+    if r, e := get_remainder(n1, n2); e != nil {
+        fmt.Println(e)
+    } else {
+        fmt.Println("The reminder is:", r)
+    }
+}
